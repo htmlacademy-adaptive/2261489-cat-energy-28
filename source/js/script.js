125c6e524fb306ff73add5f863fa8a29cc6aa5ca
Andrey Sinikov <andreysinikov91@gmail.com>
2023-05-16 14:58:35 +0300

    растягивает блок с адресом при незначительном (!) переполнении

--- source/js/script.js
+++ source/js/script.js
@@ -3,15 +3,15 @@ let navToggle = document.querySelector('.main-navigation__toggle');
 
 navMain.classList.remove('main-navigation--nojs');
 
-// navToggle.addEventListener('click', function () {
-//   if (navMain.classList.contains('main-navigation--closed')) {
-//     navMain.classList.remove('main-navigation--closed');
-//     navMain.classList.add('main-navigation--opened');
-//   } else {
-//     navMain.classList.add('main-navigation--closed');
-//     navMain.classList.remove('main-navigation--opened');
-//   }
-// });
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
 
 
 // const rangeBar = document.querySelector('.range-toggle');
